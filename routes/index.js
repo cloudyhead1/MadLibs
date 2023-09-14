c36e0d972ebad24d9ecdeed3fef96d919e7bebec
cloudyhead1 <ercx@fresnou.org>
2023-09-14 09:27:50 -0700

    commit index.js

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ function getStory(formData) {
 }
 
 function generateStory1(formData){
-  return `Twas a dark and stormy ${formData.noun1}. The ${formData.adjective1} pumpkins started to ${formData.verb1}!`
+  return `I woke up to the ${formData.adjective1} smell of turkey roasting in the room downstairs. i ${formData.verb1(past_tence)} down the stairs to see if i could help ${formData.verb1} the dinner. When i got there, I couldn't believe my eyes !`
 }
 
 function generateStory2(formData){
